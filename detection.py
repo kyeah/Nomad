3276818bef194e37a39332a22def8e1ed9980b1f
Matt Broussard <mattb@cs.utexas.edu>
2014-11-13 10:16:10 -0600

    some adapted tracking from p1

--- detection.py
+++ detection.py
@@ -1,12 +1,54 @@
 #!/usr/bin/env python
 
+# this is closely adapted from project 1
 class PlaneDetector:
 
-	def __init__(self, trainingFrame):
-		pass
+    def __init__(self, trainingFrame):
+        self.trainingFrame = trainingFrame
+        self.trainingFeatures = self.findFeatures(trainingFrame)
 
-	def detect(self, frame):
-		"""
-		Return something, probably. A homography, perhaps?
-		"""
-		pass
+    def findFeatures(self, frame):
+        kp, desc = cv2.SIFT().detectAndCompute(frame, None)
+        return kp, desc
+
+    def matchFeatures(self, descQuery, descTraining, ratio=0.7):
+        def filterKNN(matches, ratio):
+            def criterion(match):
+                a, b = match
+                return a.distance < ratio * b.distance
+            return [m[0] for m in filter(criterion, matches)]
+
+        FLANN_INDEX_KDTREE = 0
+        index_params = {
+            "algorithm": FLANN_INDEX_KDTREE,
+            "trees": 5
+        }
+        search_params = {
+            "checks": 50
+        }
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+        matches = flann.knnMatch(descQuery, descTraining, k=2)
+
+        return filterKNN(matches, ratio)
+
+    def detect(self, frame):
+        """
+        Return something, probably. A homography, perhaps?
+        """
+        kpA, descA = self.trainingFeatures
+        kbB, descB = self.findFeatures(frame)
+
+        # match the features
+        matches = []
+        for ratio in [0.7, 0.75, 0.8, 0.85]:
+            matches = self.matchFeatures(descA, descB, ratio)
+            if len(matches) >= 4:
+                break
+        assert len(matches) >= 4
+
+        # compute homography
+        srcPoints = np.array([kpB[match.queryIdx].pt for match in matches])
+        dstPoints = np.array([kpA[match.trainIdx].pt for match in matches])
+        homography, _ = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 5.0)
+
+        return homography
--- graphics.py
+++ graphics.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-def drawGrid(frame, corners):
+def drawCorners(frame, corners):
 	"""
-	Draws a grid corresponding to the rectangluar planar surface denoted by the
-	list of corners as (x, y) coordinates in the frame (unwarped). Returns the
+	Draws the corners of a tracked rectangluar planar surface denoted by the
+	list of corners as (x, y) coordinates in the frames. Returns the
 	frame (though there is no promise of not modifying the frame passed in).
 	"""
 
