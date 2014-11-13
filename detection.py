b302b22d500ea778ce12b817de836096699981f6
Matt Broussard <mattb@cs.utexas.edu>
2014-11-12 21:42:57 -0600

    some skeletons for checkpoint 1 planar impl

---
+++ detection.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+class PlaneDetector:
+
+	def __init__(self, trainingFrame):
+		pass
+
+	def detect(self, frame):
+		"""
+		Return something, probably. A homography, perhaps?
+		"""
+		pass
---
+++ graphics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+def drawGrid(frame, corners):
+	"""
+	Draws a grid corresponding to the rectangluar planar surface denoted by the
+	list of corners as (x, y) coordinates in the frame (unwarped). Returns the
+	frame (though there is no promise of not modifying the frame passed in).
+	"""
+
+	pass
