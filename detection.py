d3b4765d7fc1ead9e2f8469ececca77bac41af23
Kevin Yeh <kevinyeah@utexas.edu>
2014-11-13 21:25:03 -0600

    Arbitrary planar detection nearly working with test frame

--- detection.py
+++ detection.py
@@ -55,3 +55,47 @@ class PlaneDetector:
         homography, _ = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 5.0)
 
         return homography
+
+
+class ArbitraryPlaneDetector:
+
+    def detect(self, frame):
+        """
+        Detects arbitrary planes in a frame.
+          1. Canny Edge Detection on blurred grayscale image
+          2. Find external contours on edge image
+          3. Take the planar contour with largest estimated area
+          4. Approximate contour and fit to quad region
+        
+        """
+
+        # 1. Canny Edge Detection
+        gframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gframe = cv2.GaussianBlur(gframe, (15,15), 0)
+        edges = cv2.Canny(gframe, 50, 150)
+
+        # 2. External contours
+        contours, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+ 
+        # 3. Estimate the largest contour
+        bounds = [cv2.boundingRect(cnt) for cnt in contours]
+        areas = [b[2] * b[3] for b in bounds]
+        cnt_idx = areas.index(max(areas))
+        largestContour = contours[cnt_idx]
+
+        # Approximate contour as quad region with 4 corners
+        cntframe = np.zeros_like(gframe)
+        approxCurve = cv2.approxPolyDP(largestContour, 3, True)
+
+        # while len(approxCurve) != 4:
+        # for idx in xrange(len(approxCurve - 2)):
+        # print p1, p2, p3
+
+        cv2.drawContours(frame,approxCurve,-1,(0,0,255), 10)
+        
+        cv2.imshow('f', frame)
+        cv2.waitKey()
+        cv2.destroyAllWindows()
+
+        # Todo: approximate to 4 corners and determine clockwise order
+        return map(lambda x: x[0], approxCurve)
