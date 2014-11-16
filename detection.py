d7c1573d5ed0c9dadce59f906be344615eea737e
Kevin Yeh <kevinyeah@utexas.edu>
2014-11-16 14:28:00 -0600

    Modularize detection functionality

--- detection.py
+++ detection.py
@@ -3,6 +3,7 @@
 import cv2
 import itertools
 import numpy as np
+import vectormath as vmath
 
 # this is closely adapted from project 1
 class PlaneDetector:
@@ -58,32 +59,71 @@ class PlaneDetector:
         return homography
 
 
-def unit_vector(vector):
-    """
-    Returns the unit vector of the vector.
-    """
-    return vector / np.linalg.norm(vector)
-    
-def angle_between(v1, v2):
-    """ 
-    Returns the angle in radians between vectors 'v1' and 'v2'::
-    """
-    
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    angle = np.arccos(np.dot(v1_u, v2_u))
-    if np.isnan(angle):
-        if (v1_u == v2_u).all():
-            return 0.0
-        else:
-            return np.pi
-    return angle
-
-
-
 class ArbitraryPlaneDetector:
 
-    def detect(self, frame, same_corner_threshold_dist=20, same_side_threshold_angle=0.52, viz=False):
+    def filter_corners(self, approxCurve, alpha):
+        """
+        Removes duplicate corners by pairwise distance test.
+        """
+
+        pt_pairs = itertools.combinations(approxCurve, 2)
+        rejects = []
+            
+        for pair in pt_pairs:                
+            # Tuple conversion allows us to use 'pair in rejects' check.
+            tpair = map(lambda pt: tuple(pt), pair)
+            if tpair[0] in rejects or tpair[1] in rejects:
+                continue
+                    
+            # Distance test
+            d = np.linalg.norm(pair[1] - pair[0])
+            if d < alpha:
+                rejects.append(tpair[1])
+
+        return rejects        
+        
+    def filter_edges(self, approxCurve, beta):
+        """
+        Combines common edges by pairwise edge check.
+        Should this check be done if we already have 4 points?
+        """
+
+        pt_triplets = itertools.combinations(approxCurve, 3)
+        rejects = []
+
+        for triplet in pt_triplets:
+            tuplet = map(lambda x: tuple(x), triplet)
+            if any(map(lambda pt: pt in rejects, tuplet)):
+                continue
+
+            is_coplanar, midpt = vmath.test_coplanar(triplet, beta)
+            if is_coplanar:
+                rejects.append(tuple(midpt))
+            
+            # Short-circuit if done
+            if len(approxCurve) - len(rejects) == 4:
+                break
+
+        return rejects
+
+    def order_corners_clockwise(self, approxCurve):
+        """
+        Organize corners into clockwise rotation
+        Assume the two corners with largest yvals = top corners
+        This will fail on rotation...should only use this method to initialize.
+        Afterwards, track corners using features/small window?
+        """
+
+        y_vals = map(lambda p: p[1], approxCurve)
+        y_sorted = np.argsort(y_vals)
+
+        corners = map(lambda idx: approxCurve[idx], y_sorted)
+        c1, c2, c3, c4 = corners
+        if c1[0] > c2[0]: c1, c2 = c2, c1
+        if c4[0] > c3[0]: c3, c4 = c4, c3
+        return [c1, c2, c3, c4]
+
+    def detect(self, frame, gaussian_kernal=(15, 15), alpha=20, beta=0.52, viz=False):
         """
         Detects arbitrary planes in a frame.
           1. Canny Edge Detection on blurred grayscale image
@@ -93,128 +133,59 @@ class ArbitraryPlaneDetector:
         
         Arguments:
           frame: The frame to detect the largest plane in.
-          same_corner_threshhold_dist: Minimum distance to consider two points the same feature
-          same_side_threshold_angle: Minimum angle between two vectors to consider them the same side (default: ~30 degrees)
+          gaussian_kernal: Kernal to blur image by for Canny detection
+          alpha: Minimum distance between two points to consider them different features
+          beta: Minimum angle between two vectors to consider them different sides (default: ~30 degrees)
+          viz: Modifies the input frame to draw visual debuggers
+
+        Returns:
+          An array of x-y pairs indicating the corners in CW order, starting from top-left
         """
 
-        # TODO: Modularize mathematical computations to make code easier to read
-
         # 1. Canny Edge Detection
-        # Todo: Manual gaussian kernel modification to account for different environments
         gframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gframe = cv2.GaussianBlur(gframe, (5, 5), 0)
+        gframe = cv2.GaussianBlur(gframe, gaussian_kernal, 0)
         edges = cv2.Canny(gframe, 50, 150)
         edges = cv2.dilate(edges, (-1,-1))
 
-        # 2. External contours
+        # 2. Find External contours
         contours, hierarchy = cv2.findContours(edges,cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         if viz:
             for contour in contours:
                 cv2.drawContours(frame, contour, -1, (255, 255, 0), 10)
 
-        # 3. Estimate the most rectangular contour
-        rect = np.array([[[0, 0]], [[150, 0]], [[150, 300]], [[0, 300]]])
-
         # Return default rectangle if no contours found; todo: retain previous contour state
         if not contours:
-            return rect
+            return np.array([[0, 0], [150, 0], [150, 300], [0, 300]])
 
+        # 3. Estimate the most rectangular contour
         approxCurve = []
+        rect = np.array([[[0, 0]], [[150, 0]], [[150, 300]], [[0, 300]]])
         hu_moments = [cv2.matchShapes(contour, rect, 2, 0.0) for contour in contours]
         
         while len(approxCurve) < 4 and contours:
             cnt_idx = hu_moments.index(min(hu_moments))
+            bestContour = contours.pop(cnt_idx)
             hu_moments.pop(cnt_idx)
 
-            bestContour = contours.pop(cnt_idx)
-
-            # Approximate contour as quad region with 4 corners
-            approxCurve = cv2.approxPolyDP(bestContour, epsilon=3, closed=True)
-            
             if viz:
                 cv2.drawContours(frame, bestContour, -1, (0, 255, 0), 10)
             
+            # Approximate contour as a polygon
+            approxCurve = cv2.approxPolyDP(bestContour, epsilon=3, closed=True)            
             approxCurve = map(lambda x: x[0], approxCurve)
             
-            # Pairwise distance check; rejects contain rejected points.
-            # Tuple conversion allows us to use 'pair in rejects' check.
-            pt_pairs = itertools.combinations(approxCurve, 2)
-            rejects = []
-            
-            for pair in pt_pairs:
-                tpair = map(lambda pt: tuple(pt), pair)
-                if tpair[0] in rejects or tpair[1] in rejects:
-                    continue
-                    
-                # Distance test
-                d = np.linalg.norm(pair[1] - pair[0])
-                if d < same_corner_threshold_dist:
-                    rejects.append(tpair[1])
-
+            rejects = self.filter_corners(approxCurve, alpha)
             approxCurve = np.array([pt for pt in approxCurve if tuple(pt) not in rejects])
 
         if viz:
             for pt in approxCurve:
                 cv2.circle(frame, tuple(pt), 4, (255, 0, 0))
 
-        # Check each pair of connected sides for commonality.
-        # Again, tuple conversion allows us to check array containment easily.
-        # Should this check be done if we already have 4 points? Maybe. Might get rid of false quads.
-        pt_triplets = itertools.combinations(approxCurve, 3)
-        rejects = []
-            
-        for triplet in pt_triplets:
-            tuplet = map(lambda x: tuple(x), triplet)
-            if any(map(lambda pt: pt in rejects, tuplet)):
-                continue
-                
-            p1, p2, p3 = triplet
-            v1, v2 = unit_vector(p2 - p1), unit_vector(p3 - p2)
-            angle = angle_between(v1, v2)
-
-            # Angle, Reverse-angle Test
-            # If the three lines are roughly coplanar, find the longest line and remove the middle point.
-            if angle < same_side_threshold_angle or abs(np.pi - angle) < same_side_threshold_angle:
-                distances = map(lambda idx: np.linalg.norm((idx+1) % 3 - (idx+2) % 3), xrange(3))
-                rejects.append(distances.index(max(distances)))
-            
-            # Short-circuit if done
-            if len(approxCurve) - len(rejects) == 4:
-                break
-
+        # Filter edges and approximate to best quadrilateral
+        rejects = self.filter_edges(approxCurve, beta)
         approxCurve = np.array([pt for pt in approxCurve if tuple(pt) not in rejects])
+        approxCurve = vmath.approx_quadrilateral(approxCurve, alpha)
 
-        if len(approxCurve) == 2:
-            # Find dx, dy, and infer head-on plane if the two corners look like they are opposite from each other
-            c1, c2 = approxCurve[0], approxCurve[1]
-            dx, dy = c2[0] - c1[0], c2[1] - c1[1]
-            if abs(dx) > same_corner_threshold_dist / 4 and abs(dy) > same_corner_threshold_dist / 4:
-                if dx < 0: c2, c1 = c1, c2
-                approxCurve = np.vstack([approxCurve, [[c1[0] + dx, c1[1]], [c1[0], c1[1] + dy]]])
-
-        elif len(approxCurve) == 3:
-            # Find hypotenuse, then find last corner
-            c1, c2, c3 = approxCurve[0], approxCurve[1], approxCurve[2]
-            d1, d2, d3 = np.linalg.norm(c2 - c1), np.linalg.norm(c3 - c1), np.linalg.norm(c3 - c2)
-            # todo: ...finish this
-
-        # Last resorts: if not enough points, replicate; if too many points, truncate.
-        while len(approxCurve) < 4:
-            approxCurve = np.vstack([approxCurve, approxCurve[len(approxCurve) - 1]])
-
-        approxCurve = approxCurve[:4]
-
-        # Organize corners into clockwise rotation
-        # Assume the two corners with largest yvals = top corners
-        # This will fail on rotation...should only use this method to initialize.
-        # Afterwards, track using features?
-        y_vals = map(lambda p: p[1], approxCurve)
-        y_sorted = np.argsort(y_vals)
-
-        corners = map(lambda idx: approxCurve[idx], y_sorted)
-        c1, c2, c3, c4 = corners
-        if c1[0] > c2[0]: c1, c2 = c2, c1
-        if c4[0] > c3[0]: c3, c4 = c4, c3
-
-        return [c1, c2, c3, c4]
+        return self.order_corners_clockwise(approxCurve)
---
+++ vectormath.py
@@ -0,0 +1,63 @@
+import numpy as np
+
+def unit_vector(vector):
+    """
+    Returns the unit vector of the vector.
+    """
+    return vector / np.linalg.norm(vector)
+    
+
+def angle_between(v1, v2):
+    """ 
+    Returns the angle in radians between vectors 'v1' and 'v2'::
+    """
+    
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    angle = np.arccos(np.dot(v1_u, v2_u))
+    if np.isnan(angle):
+        if (v1_u == v2_u).all():
+            return 0.0
+        else:
+            return np.pi
+    return angle
+
+
+def test_coplanar(triplet, beta=0.52):
+    p1, p2, p3 = triplet
+    v1, v2 = unit_vector(p2 - p1), unit_vector(p3 - p2)
+    angle = angle_between(v1, v2)
+
+    # Angle, Reverse-angle Test
+    # If the three lines are roughly coplanar, find the longest line and return the middle point.
+    if angle < beta or abs(np.pi - angle) < beta:
+        distances = map(lambda idx: np.linalg.norm(triplet[(idx+1) % 3] - triplet[(idx+2) % 3]), xrange(3))
+        return True, triplet[distances.index(max(distances))]
+
+    return False, None
+
+def approx_quadrilateral(corners, alpha):
+    """
+    Approximate missing corners of quadrilateral. Modifies the input curve.
+    """
+
+    if len(corners) == 2:
+        # Find dx, dy, and infer head-on plane if the two corners look like they are opposite from each other
+        c1, c2 = corners[0], corners[1]
+        dx, dy = c2[0] - c1[0], c2[1] - c1[1]
+        if abs(dx) > alpha and abs(dy) > alpha:
+            if dx < 0: c2, c1 = c1, c2
+            corners = np.vstack([corners, [[c1[0] + dx, c1[1]], [c1[0], c1[1] + dy]]])
+
+    elif len(corners) == 3:
+        # Find hypotenuse, then estimate last corner
+        c1, c2, c3 = corners[0], corners[1], corners[2]
+        d1, d2, d3 = np.linalg.norm(c2 - c1), np.linalg.norm(c3 - c1), np.linalg.norm(c3 - c2)
+        # todo: ...finish this
+
+    # Last resorts: if not enough points, replicate; if too many points, truncate.
+    while len(corners) < 4:
+        corners = np.vstack([corners, corners[len(corners) - 1]])
+
+    corners = corners[:4]
+    return corners
