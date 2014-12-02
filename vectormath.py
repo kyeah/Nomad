46716d3ddb3271180ac38d8d2593422bbf9c4d1f
Conner Collins <ConnerCollins@utexas.edu>
2014-12-01 21:49:00 -0600

    vectormath.py pep8

--- vectormath.py
+++ vectormath.py
@@ -1,17 +1,18 @@
 import numpy as np
 
+
 def unit_vector(vector):
     """
     Returns the unit vector of the vector.
     """
     return vector / np.linalg.norm(vector)
-    
+
 
 def angle_between(v1, v2):
-    """ 
+    """
     Returns the angle in radians between vectors 'v1' and 'v2'::
     """
-    
+
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     angle = np.arccos(np.dot(v1_u, v2_u))
@@ -29,12 +30,15 @@ def test_coplanar(triplet, beta=0.52):
     angle = angle_between(v1, v2)
 
     # Angle, Reverse-angle Test
-    # If the three lines are roughly coplanar, find the longest line and return the middle point.
+    # If the three lines are roughly coplanar, find the
+    # longest line and return the middle point.
     if angle < beta or abs(np.pi - angle) < beta:
-        distances = map(lambda idx: np.linalg.norm(triplet[(idx+1) % 3] - triplet[(idx+2) % 3]), xrange(3))
+        distances = map(lambda idx: np.linalg.norm(
+            triplet[(idx+1) % 3] - triplet[(idx+2) % 3]), xrange(3))
         return True, triplet[distances.index(max(distances))]
 
     return False, None
+
 
 def approx_quadrilateral(corners, alpha):
     """
@@ -42,20 +46,25 @@ def approx_quadrilateral(corners, alpha):
     """
 
     if len(corners) == 2:
-        # Find dx, dy, and infer head-on plane if the two corners look like they are opposite from each other
+        # Find dx, dy, and infer head-on plane if the two corners
+        # look like they are opposite from each other
         c1, c2 = corners[0], corners[1]
         dx, dy = c2[0] - c1[0], c2[1] - c1[1]
         if abs(dx) > alpha and abs(dy) > alpha:
-            if dx < 0: c2, c1 = c1, c2
-            corners = np.vstack([corners, [[c1[0] + dx, c1[1]], [c1[0], c1[1] + dy]]])
+            if dx < 0:
+                c2, c1 = c1, c2
+            corners = np.vstack(
+                [corners, [[c1[0] + dx, c1[1]], [c1[0], c1[1] + dy]]])
 
     elif len(corners) == 3:
         # Find hypotenuse, then estimate last corner
         c1, c2, c3 = corners[0], corners[1], corners[2]
-        d1, d2, d3 = np.linalg.norm(c2 - c1), np.linalg.norm(c3 - c1), np.linalg.norm(c3 - c2)
+        d1, d2, d3 = np.linalg.norm(
+            c2 - c1), np.linalg.norm(c3 - c1), np.linalg.norm(c3 - c2)
         # todo: ...finish this
 
-    # Last resorts: if not enough points, replicate; if too many points, truncate.
+    # Last resorts: if not enough points, replicate;
+    # if too many points, truncate.
     while len(corners) < 4:
         corners = np.vstack([corners, corners[len(corners) - 1]])
 
