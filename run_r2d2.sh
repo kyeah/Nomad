b4dd7a03c1a1671b0c2818c4a1783041153f55a1
kyeah <kevinyeah@utexas.edu>
2014-12-02 14:13:21 -0600

    Added r2d2 content

---
+++ run_r2d2.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Paint on R2D2! Draw with the mouse, then press 'p' to start tracking the painted object. Press 'l' to play the full video.
+
+python track_planar.py \
+    --stream \
+    --viz \
+    --stall \
+    content/r2d2.mp4
+    "$@"
